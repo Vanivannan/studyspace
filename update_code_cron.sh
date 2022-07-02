6b3c8f96294e0b0033ce36206dd186c1b25cd7f4
Vanivannan <776169038@qq.com>
2022-07-02 21:53:33 +0800

    update file

--- update_code_cron.sh
+++ update_code_cron.sh
@@ -9,7 +9,7 @@ git stash
 git pull -u origin master
 echo `date`>>tip.txt
 git status
-git add .
+git add all
 git commit -m "update file"
 git push -u origin master
 echo '***********************************END**************************************************'
