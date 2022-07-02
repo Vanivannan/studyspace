5dbc7d1b53b3524048248edaa517434e142810ca
Vanivannan <776169038@qq.com>
2022-07-02 20:12:08 +0800

    update file

---
+++ update_code_cron.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+export LANG=zh_CN.UTF-8
+
+cd /opt/opensource/studyspace
+git stash
+git pull -u origin master
+echo `date`>>tip.txt
+git status
+git add .
+git commit -m "update file"
+git push -u origin master
+
