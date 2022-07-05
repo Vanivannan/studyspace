8ddd89236a10ae0e248f8f242bc01cf7f1ae3164
Vanivannan <776169038@qq.com>
2022-07-05 23:00:01 +0800

    update file

--- update_code_cron.sh
+++ update_code_cron.sh
@@ -3,17 +3,13 @@ export LANG=zh_CN.UTF-8
 
 # 0,30 * * * * /bin/sh /opt/opensource/studyspace/update_code_cron.sh 2>&1 >/opt/opensource/studyspace/$(date +\%Y\%m\%d).log
 echo ''
-echo "update time: `date`"
-echo '**********************************START************************************************' 1>&2
+echo "update time is: `date`"
+echo '**********************************************************************************' 1>&2
 cd /opt/opensource/studyspace
-#git stash
-#git pull -u origin master
-echo `date`>>tip.txt
-git add --all
-#git add .
-git commit -m "update file"
+echo `date` >>tip.txt
+git add --all && git commit -m "update file"
 git push -u origin master
 git stash
 git pull -u origin master
 git status
-echo '***********************************END**************************************************' 1>&2
+echo '**********************************************************************************' 1>&2
