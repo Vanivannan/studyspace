6755d616450f532cade247d968ade192e2cff6ef
Dony <776169038@qq.com>
2022-07-05 23:10:03 +0800

    update code

--- update_code_cron.sh
+++ update_code_cron.sh
@@ -1,11 +1,12 @@
 #!/bin/sh
 export LANG=zh_CN.UTF-8
+source_path=/opt/opensource/studyspace
 
 # 0,30 * * * * /bin/sh /opt/opensource/studyspace/update_code_cron.sh 2>&1 >/opt/opensource/studyspace/$(date +\%Y\%m\%d).log
 echo ''
 echo "update time is: `date`"
 echo '**********************************************************************************'
-cd /opt/opensource/studyspace
+cd $source_path
 echo `date` >>tip.txt
 git add --all && git commit -m "update file"
 git push -u origin master
