101040e7aad6165f5a30a40af6661c77e0bbf042
Vanivannan <776169038@qq.com>
2022-12-08 22:37:22 +0800

    update file

--- update_code_cron.sh
+++ update_code_cron.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 export LANG=zh_CN.UTF-8
-source_path=/opt/opensource/studyspace
+source_path=/opt/projects/studyspace
 
-# 0,30 * * * * /bin/sh /opt/opensource/studyspace/update_code_cron.sh 2>&1 >/opt/opensource/studyspace/$(date +\%Y\%m\%d).log
+# 0,30 * * * * /bin/sh /opt/projects/studyspace/update_code_cron.sh 2>&1 >/opt/opensource/studyspace/$(date +\%Y\%m\%d).log
 echo ''
 echo "update time is: `date`"
 echo '**********************************************************************************'
